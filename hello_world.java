31bad570261adf0c3c7ec4dbc6278b2dcf4eeb2e
mriol94 <manelriol94@gmail.com>
2015-10-07 19:09:46 +0200

    v1.HelloWorld

--- hello_world.java
+++ hello_world.java
@@ -1,5 +1,5 @@
 public class Hello {
   public static void main(String[] args) {
-    System.out.println("Hola mundo");
+    System.out.println("Hola mundo : To forgive all is as inhuman as to forgive none (Aristòtil). Fortunately we have GIT");
   }
 }
